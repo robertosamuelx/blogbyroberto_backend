cadf7ce8e1c9d502aeed0eab8bf22167c0bbdcac
robertosamuelx <roberto-samuel1@hotmail.com>
2020-05-05 20:41:55 -0300

    criação do envio de e-mail

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-Projeto para o blog do meu pai
+# BackEnd #
+---
+**Objetivo:**
+- Desenvolver o back end do blog para o professor Roberto Maia
+
+**Tecnologias envolvidas:**
+- Nodejs v12.16.1
+
+**Dependências:**
+- Cors v2.8.5
+- Dotenv v8.2
+- Express v4.17.1
+- Jsonwebtoken v8.5.1
+- Mongoose v5.9.9
+
+**Dependencias de Desenvolvimento:**
+- Nodemon v2.0.3
+
+**Hospedagem:**
+- [Heroku](https://www.heroku.com/)
+
+**Como usar:**
+- [Para clonar o projeto clique aqui](https://github.com/robertosamuelx/blogbyroberto_backend.git)
+- Yarn i
+- Yarn start
+(O Yarn pode ser substituído por outro package manager, como NPM)
+
+*God be praised*
--- src/controllers/ContactController.js
+++ src/controllers/ContactController.js
@@ -1,5 +1,38 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+    service: process.env.EMAIL_PROVIDER,
+    auth: {
+        user: process.env.EMAIL_AUTH_USER,
+        pass: process.env.EMAIL_AUTH_PASS
+    }
+});
+
 module.exports = {
-    send(req,res){
-        return res.status(200).json({message:"Done!"});
+    async send(req,res){
+        const {name,email,message} = req.body;
+
+        const mailOptions = {
+            from: process.env.EMAIL_AUTH_USER,
+            to: process.env.EMAIL_AUTH_USER,
+            subject: 'Você recebeu uma nova mensagem!',
+            text: `Você recebeu uma mensagem de ${name}.\n"${message}".\nPara responder, envie um e-mail para ${email}`
+        };
+
+        let response, status;
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if(error){
+                response = `Ops! Houve uma falha no envio.\tPor favor ${name} tente novamente mais tarde.`;
+                status = 400;
+                return res.status(status).json({message:response});
+            }
+            
+            else {
+               response = `Feito! Fique atento a sua caixa de e-mails ${name}\tpois em breve retornarei o contato.`;
+               status = 200;
+               return res.status(status).json({message:response});
+            }
+        });
     }
 }
